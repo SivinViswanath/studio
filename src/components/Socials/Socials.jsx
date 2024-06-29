064de4ffedfe5b1e3032e9e4626b3b2beaf32be0
SivinViswanath <sivinviswanath99@gmail.com>
2024-06-29 22:47:22 +0530

    icon sizes changed

--- src/components/Socials/Socials.jsx
+++ src/components/Socials/Socials.jsx
@@ -11,29 +11,29 @@ import { SiYoutubemusic } from 'react-icons/si';
 const Socials = () => {
   return (
     <section>
-      <div className="my-4 md:my-52 bg-gray-900 grid grid-cols-3 md:grid-cols-6 justify-items-center items-center p-7 md:p-20 gap-6">
+      <div className="my-4 md:my-52 bg-gray-900 grid grid-cols-3 md:grid-cols-6 justify-items-center items-center p-7 md:p-20 gap-2">
         <FaFacebookSquare
           cursor="pointer"
-          className="w-12 h-16 md:w-24 md:h-24 hover-bounce  text-indigo-300"
+          className="w-12 h-16 md:w-15 md:h-15 hover-bounce  text-indigo-300"
         />
         <FaInstagram
-          className="w-12 h-16 md:w-24 md:h-24 hover-bounce text-indigo-300"
+          className="w-12 h-16 md:w-15 md:h-15 hover-bounce text-indigo-300"
           cursor="pointer"
         />
         <FaYoutube
-          className="w-12 h-16 md:w-24 md:h-24 hover-bounce  text-indigo-300"
+          className="w-12 h-16 md:w-15 md:h-15 hover-bounce  text-indigo-300"
           cursor="pointer "
         />
         <FaSpotify
-          className="w-12 h-16 md:w-24 md:h-24 hover-bounce  text-indigo-300"
+          className="w-12 h-16 md:w-15 md:h-15 hover-bounce  text-indigo-300"
           cursor="pointer"
         />
         <FaTwitter
-          className="w-12 h-16 md:w-24 md:h-24 hover-bounce  text-indigo-300"
+          className="w-12 h-16 md:w-15 md:h-15 hover-bounce  text-indigo-300"
           cursor="pointer"
         />
         <SiYoutubemusic
-          className="w-12 h-16 md:w-24 md:h-24 hover-bounce  text-indigo-300"
+          className="w-12 h-16 md:w-15 md:h-15 hover-bounce  text-indigo-300"
           cursor="pointer"
         />
       </div>
